01a51a17b15ec34ca5debdfa45c8edee45937b93
Michał Kacprzak <michalkacprzak54@wp.pl>
2025-01-13 17:51:22 +0100

    +post in controller and API endpoint

--- Controllers/ServiceScheduleController.cs
+++ Controllers/ServiceScheduleController.cs
@@ -28,13 +28,51 @@ namespace DrivingSchoolAPI.Controllers
         public async Task<ActionResult<IEnumerable<ServiceSchedule>>> GetServicesSchedule()
         {
             var servicesSchedule = await _context.ServiceSchedules
+                .ToListAsync();
+
+            return Ok(servicesSchedule);
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<ServiceSchedule>>> GetServiceSchedule(int id)
+        {
+            var servicesSchedule = await _context.ServiceSchedules
                 .Include(ss => ss.ClientService)
                 .Include(ss => ss.PraticeSchedule)
                 .Include(ss => ss.Status)
-                .ToListAsync();
+                .Where(ss => ss.IdServiceSchedule == id)
+                .FirstAsync();
 
             return Ok(servicesSchedule);
         }
 
+        [HttpPost]
+        public async Task<ActionResult<ServiceSchedule>> PostServiceSchedule(ServiceSchedule serviceSchedule)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                await _context.Database.ExecuteSqlRawAsync(
+                    "EXEC DodajUslugaHarmonogram @IdKlientUsluga = {0}, @IdHarmonogram = {1}, @DataRezerwacji = {2}, @IdStatus = {3}",
+                    serviceSchedule.IdClientService,
+                    serviceSchedule.IdPraticeSchedule,
+                    serviceSchedule.ReservationDate,
+                    serviceSchedule.IdStatus
+                    );
+                return CreatedAtAction("GetServiceSchedule", new { id = serviceSchedule.IdServiceSchedule }, serviceSchedule);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return StatusCode(500, "Wystąpił błąd podczas zapisywania danych.");
+            }
+
+
+
+        }
     }
 }
